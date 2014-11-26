4057fc5b04987fdfd2b80405a08cf6272e9610b8
Shallker <imshallker@gmail.com>
2014-11-26 13:43:36 +0800

    Update extend

--- src/extend.js
+++ src/extend.js
@@ -8,8 +8,8 @@ define('util:extend', function (require, exports, module) {
      * 将`targetObject`上的所有属性移植到`sourceObject`上
      * 在移植时，如果`sourceObject`上存在相同的属性或方法，则覆盖
      * 
-     * @param  {Function} sourceObject   基对象
-     * @param  {Function} targetObject 扩展对象
+     * @param  {Object} sourceObject 源对象
+     * @param  {Object} targetObject 目标对象
      * @return {undefined}
      */
     function extend(sourceObject, targetObject) {
